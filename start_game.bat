fd9c3c945b46d963fc2d23501d7325480c4c5fa7
Daniel <denialmc@outlook.com>
2015-03-08 00:06:21 +0200

    Add new batch game launcher.

--- start_game.bat
+++ start_game.bat
@@ -1,23 +1,46 @@
 @echo off
-
-rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+title TTU Game Launcher
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
 rem Get the user input:
-set /P ttuUsername="Username: "
-set /P TTU_GAMESERVER="Gameserver (DEFAULT: 167.114.28.238): " || ^
-set TTU_GAMESERVER=167.114.28.238
+echo Choose your game server!
+echo #1 - Localhost
+echo #2 - DenialMC
+echo #3 - Custom
+echo #4 - Custom w/ RemoteDB
+echo.
+set /P INPUT=
+set TTU_GAMESERVER=unset
 
-rem Export the environment variables:
-set ttuPassword=password
+if %INPUT%==1 set TTU_GAMESERVER=127.0.0.1
+if %INPUT%==2 set TTU_GAMESERVER=5.15.21.225
+if %TTU_GAMESERVER%==unset (
+    echo.
+    set /P TTU_GAMESERVER=Gameserver: 
+)
+
+echo.
+set /P ttuUsername=Username: 
+
+if %INPUT%==4 (
+    echo.
+    set /P ttuPassword=Password: 
+)
+
 set TTU_PLAYCOOKIE=%ttuUsername%
 
 echo ===============================
 echo Starting Toontown Unlimited...
 echo ppython: %PPYTHON_PATH%
 echo Username: %ttuUsername%
+echo Password: %ttuPassword%
 echo Gameserver: %TTU_GAMESERVER%
 echo ===============================
 
-%PPYTHON_PATH% -m toontown.toonbase.ClientStart
+if %INPUT%==4 (
+    %PPYTHON_PATH% -m toontown.toonbase.ClientStartRemoteDB
+) else (
+    %PPYTHON_PATH% -m toontown.toonbase.ClientStart
+)
+
 pause
