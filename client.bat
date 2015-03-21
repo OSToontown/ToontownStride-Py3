5bc056d63054c8269eee981c8d41d87e74079639
Daniel <denialmc@outlook.com>
2015-03-21 14:02:05 +0200

    My last commit on here

--- client.bat
+++ client.bat
@@ -4,17 +4,13 @@ set /P PPYTHON_PATH=<PPYTHON_PATH
 
 echo Choose your game server!
 echo #1 - Localhost
-echo #2 - DenialMC
-echo #3 - Development Server
-echo #4 - Custom
-echo #5 - Custom w/ RemoteDB
+echo #2 - Custom
+echo #3 - Custom w/ RemoteDB
 echo.
 set /P INPUT=
 set TTU_GAMESERVER=unset
 
 if %INPUT%==1 set TTU_GAMESERVER=127.0.0.1
-if %INPUT%==2 set TTU_GAMESERVER=5.15.21.225
-if %INPUT%==3 set TTU_GAMESERVER=23.92.75.62
 if %TTU_GAMESERVER%==unset (
     echo.
     set /P TTU_GAMESERVER=Gameserver: 
@@ -23,7 +19,7 @@ if %TTU_GAMESERVER%==unset (
 echo.
 set /P ttuUsername=Username: 
 
-if %INPUT%==5 (
+if %INPUT%==3 (
     echo.
     set /P ttuPassword=Password: 
 )
@@ -38,7 +34,7 @@ echo Password: %ttuPassword%
 echo Gameserver: %TTU_GAMESERVER%
 echo ===============================
 
-if %INPUT%==5 (
+if %INPUT%==3 (
     %PPYTHON_PATH% -m toontown.toonbase.ClientStartRemoteDB
 ) else (
     %PPYTHON_PATH% -m toontown.toonbase.ClientStart
