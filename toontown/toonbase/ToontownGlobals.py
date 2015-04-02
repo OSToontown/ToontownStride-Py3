233dbd4ba924df1833def47c53f430affb8b80b6
Loudrob <loudhuddles131@yahoo.com>
2015-04-02 08:23:24 -0400

    sequence asslist and chathoes

--- toontown/toonbase/ToontownGlobals.py
+++ toontown/toonbase/ToontownGlobals.py
@@ -178,7 +178,7 @@ HoodHierarchy = {ToontownCentral: (SillyStreet, LoopyLane, PunchlinePlace),
  TheBrrrgh: (WalrusWay, SleetStreet, PolarPlace),
  MinniesMelodyland: (AltoAvenue, BaritoneBoulevard, TenorTerrace),
  DaisyGardens: (ElmStreet, MapleStreet, OakStreet),
- DonaldsDreamland: (LullabyLane, PajamaPlace, RockAByeRoad),
+ DonaldsDreamland: (LullabyLane, PajamaPlace),
  GoofySpeedway: ()}
 WelcomeValleyToken = 0
 BossbotHQ = 10000
