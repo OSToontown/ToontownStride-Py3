16dfbb003b15699de34dd575a11fb324190f4165
DenialMC <denialmc@outlook.com>
2015-04-08 20:19:02 +0300

    More stuff

--- toontown/ai/DistributedReportMgrAI.py
+++ toontown/ai/DistributedReportMgrAI.py
@@ -1,32 +1,31 @@
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.DistributedObjectAI import DistributedObjectAI
 from toontown.uberdog.ClientServicesManagerUD import executeHttpRequestAndLog
-import ReportGlobals, threading
+import ReportGlobals, threading, time
 
 class DistributedReportMgrAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedReportMgrAI")
 
     def __init__(self, air):
-        DistributedObjectAI.DistributedObjectAI(self, air)
+        DistributedObjectAI.__init__(self, air)
         self.reports = []
         self.interval = config.GetInt('report-interval', 600)
         self.scheduleReport()
     
     def scheduleReport(self):
-        threading.Timer(self.interval, self.sendAllReports)
+        threading.Timer(self.interval, self.sendAllReports).start()
     
     def sendReport(self, avId, category):
         if not ReportGlobals.isValidCategoryName(category) or not len(str(avId)) == 9:
             return
         
-        reporterId = self.air.getAvatarIdFromSender()
-        reporter = self.air.doId2do.get(reporterId)
+        reporter = self.air.doId2do.get(self.air.getAvatarIdFromSender())
         
         if not reporter or reporter.isReported(avId):
             return
         
         timestamp = int(round(time.time() * 1000))
-        self.reports.append('%s|%s|%s|%s' % (timestamp, reporterId, avId, category))
+        self.reports.append('%s|%s|%s|%s' % (timestamp, reporter.doId, avId, category))
     
     def sendAllReports(self):
         if not self.reports or config.GetString('accountdb-type', 'developer') != 'remote':
