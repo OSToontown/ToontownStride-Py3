9cec2ff13b46aee2d457a07ff7277f6d3b2f8da7
DenialMC <denialmc@outlook.com>
2015-05-06 11:11:02 +0300

    So turns out we can't just remove this, because then we would have to put the whole DistributedObjectAI in the dc file.. thank you Joey and the TTR team

--- otp/distributed/DistributedDirectoryAI.py
+++ otp/distributed/DistributedDirectoryAI.py
@@ -3,7 +3,3 @@ from direct.distributed.DistributedObjectAI import DistributedObjectAI
 
 class DistributedDirectoryAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedDirectoryAI")
-
-    def setParentingRules(self, todo0, todo1):
-        pass
-
