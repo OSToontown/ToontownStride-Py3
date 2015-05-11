c2920fae26c69324d4a07205bba27005903a6368
John Cote <jwcotejr@comcast.net>
2015-05-11 16:13:24 -0400

    Fix parse_dcimports.bat

--- tools/parse_dcimports.bat
+++ tools/parse_dcimports.bat
@@ -1,3 +1,5 @@
-set /P PPYTHON_PATH=<PPYTHON_PATH
+set /P PPYTHON_PATH=<../PPYTHON_PATH
 
-%PPYTHON_PATH% parse_dcimports.py -o "otp/distributed/DCClassImports.py" "astron/dclass/united.dc"
+cd..
+
+%PPYTHON_PATH% tools\parse_dcimports.py -o "otp/distributed/DCClassImports.py" "astron/dclass/united.dc"
