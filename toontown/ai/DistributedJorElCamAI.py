d4974e8d540cadbda010151efc6160e2ca1a9796
Zach <zachgatestech@gmail.com>
2015-05-10 22:53:28 -0500

    Jor-El Cam

---
+++ toontown/ai/DistributedJorElCamAI.py
@@ -0,0 +1,24 @@
+from direct.directnotify import DirectNotifyGlobal
+from direct.distributed.DistributedObjectAI import DistributedObjectAI
+from toontown.toon import ToonDNA
+
+class DistributedJorElCamAI(DistributedObjectAI):
+    notify = DirectNotifyGlobal.directNotify.newCategory("DistributedJorElCamAI")
+
+    def __init__(self, air):
+        DistributedObjectAI.__init__(self, air)
+        self.air = air
+        self.dna = None
+
+    def generate(self):
+        self.dna = ToonDNA.ToonDNA()
+        self.dna.newToonRandom()
+        DistributedObjectAI.generate(self)
+
+    def delete(self):
+        del self.dna
+        DistributedObjectAI.delete(self)
+
+    def disable(self):
+        del self.dna
+        DistributedObjectAI.disable(self)
