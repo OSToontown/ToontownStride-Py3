ab3876ff0d1ce0b9c3e7de6ecafb4a441e6970be
Zach <zachgatestech@gmail.com>
2015-05-12 15:52:38 -0500

    DistributedGardenAI is done

--- toontown/estate/DistributedGardenAI.py
+++ toontown/estate/DistributedGardenAI.py
@@ -5,8 +5,31 @@ class DistributedGardenAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedGardenAI")
 
     def __init__(self, air):
+        DistributedObjectAI.__init__(self, air)
         self.air = air
         self.props = []
 
-    def sendNewProp(self, prop, x, y, z):
-        self.props.append([prop, x, y, z])
+    def generate(self):
+        DistributedObjectAI.generate(self)
+
+    def announceGenerate(self):
+        DistributedObjectAI.announceGenerate(self)
+
+    def disable(self):
+        DistributedObjectAI.disable(self)
+
+    def delete(self):
+        DistributedObjectAI.delete(self)
+
+    def b_setProps(self, props):
+        self.setProps(props)
+        self.d_setProps(props)
+
+    def d_setProps(self, props):
+        aProps = []
+        for prop in props:
+            aProps = aProps + prop
+        self.sendUpdate('setProps', [aProps])
+
+    def setProps(self, props):
+        self.props = props
