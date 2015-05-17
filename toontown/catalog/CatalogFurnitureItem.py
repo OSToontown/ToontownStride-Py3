88606b387655fe854f85ed89dab11f3a1efbcf94
John Cote <jwcotejr@comcast.net>
2015-05-17 00:52:14 -0400

    Revert "No more accessory, clothing, furniture item gender discrimination! (except for skirts)"
    This reverts commit c866a4a52ff1b0febbb10312cffe607335f578c1.

--- toontown/catalog/CatalogFurnitureItem.py
+++ toontown/catalog/CatalogFurnitureItem.py
@@ -18,6 +18,8 @@ FLIsTable = 32
 FLPhone = 64
 FLBillboard = 128
 FLTrunk = 256
+FLBoysOnly = 512
+FLGirlsOnly = 1024
 furnitureColors = [
   (0.792, 0.353, 0.29, 1.0),
   (0.176, 0.592, 0.439, 1.0),
@@ -161,7 +163,8 @@ FurnitureTypes = {
  210: ('phase_5.5/models/estate/girly_bed',
        None,
        None,
-       450),
+       450,
+       FLGirlsOnly),
  220: ('phase_5.5/models/estate/bathtub_bed',
        None,
        None,
@@ -205,7 +208,8 @@ FurnitureTypes = {
  410: ('phase_5.5/models/estate/FireplaceGirlee',
        None,
        None,
-       800),
+       800,
+       FLGirlsOnly),
  420: ('phase_5.5/models/estate/FireplaceRound',
        None,
        None,
@@ -247,7 +251,7 @@ FurnitureTypes = {
        None,
        None,
        1100,
-	   None,
+       FLGirlsOnly,
        None,
        0.5),
  491: ('phase_5.5/models/estate/tt_m_prp_int_fireplace_bugRoom',
@@ -913,7 +917,23 @@ class CatalogFurnitureItem(CatalogAtticItem.CatalogAtticItem):
                 return not forBoys
             else:
                 return forBoys
+        if self.forBoysOnly():
+            if avatar.getStyle().getGender() == 'm':
+                return 0
+            else:
+                return 1
+        elif self.forGirlsOnly():
+            if avatar.getStyle().getGender() == 'f':
+                return 0
+            else:
+                return 1
         return 0
+
+    def forBoysOnly(self):
+        return self.getFlags() & FLBoysOnly > 0
+
+    def forGirlsOnly(self):
+        return self.getFlags() & FLGirlsOnly > 0
 
     def isDeletable(self):
         return self.getFlags() & (FLBank | FLCloset | FLPhone | FLTrunk) == 0
