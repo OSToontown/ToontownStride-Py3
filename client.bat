f71c3dbe70a4970a6a87c991956906c641785337
Master Voltage <lightzingvolt@gmail.com>
2015-05-16 23:21:32 -0400

    added dev server to client

--- client.bat
+++ client.bat
@@ -4,14 +4,16 @@ set /P PPYTHON_PATH=<PPYTHON_PATH
 
 echo Choose your game server!
 echo #1 - Localhost
-echo #2 - Custom
-echo #3 - Loudrob
+echo #2 - Loudrob
+echo #3 - Developer Server (Steve)
+echo #4 - Custom
 echo.
 set /P INPUT=
 set server=unset
 
 if %INPUT%==1 set TTS_GAMESERVER=127.0.0.1
-if %INPUT%==3 set TTS_GAMESERVER=71.200.196.180
+if %INPUT%==2 set TTS_GAMESERVER=71.200.196.180
+if %INPUT%==3 set TTS_GAMESERVER=192.99.167.192
 
 if %TTS_GAMESERVER%==unset (
     echo.
