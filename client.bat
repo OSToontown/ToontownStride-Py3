09169b091ea8df283091c808e7911fc55b90f713
stevetts <ttinfsteve@gmail.com>
2015-05-22 22:57:40 -0400

    New Server
    Changed steve's server to new server

--- client.bat
+++ client.bat
@@ -5,13 +5,15 @@ set /P PPYTHON_PATH=<PPYTHON_PATH
 echo Choose your game server!
 echo #1 - Localhost
 echo #2 - Loudrob
-echo #3 - Custom
+echo #3 - Steve
+echo #4 - Custom
 echo.
 set /P INPUT=
 
 if %INPUT%==1 set TTS_GAMESERVER=127.0.0.1
 if %INPUT%==2 set TTS_GAMESERVER=71.200.196.180
-if %INPUT%==3 (
+if %INPUT%==3 set TTS_GAMESERVER=167.114.220.172
+if %INPUT%==4 (
     echo.
     set /P TTS_GAMESERVER=Gameserver: 
 )
