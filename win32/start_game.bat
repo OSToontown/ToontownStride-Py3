5fd23d0933a484779b1c30093f7f68aeedb73d39
John Cote <jwcotejr@comcast.net>
2015-05-23 00:53:15 -0400

    Remove start_game_remotedb.bat and edit start_game.bat

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -1,36 +1,57 @@
 @echo off
 cd..
 
-title TTS Game Launcher
+title Toontown Stride Game Launcher
 
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
-echo Choose your game server!
-echo #1 - Localhost
-echo #2 - Custom
-echo #3 - Loudrob
+echo Choose your connection method!
 echo.
-set /P INPUT=
-set TTS_GAMESERVER=unset
+echo #1 - Localhost
+echo #2 - Dev Server
+echo #3 - Custom
+echo.
 
-if %INPUT%==1 set TTS_GAMESERVER=127.0.0.1
-if %INPUT%==3 set TTS_GAMESERVER=71.200.196.180
+set /P INPUT=Selection: 
 
-if %TTS_GAMESERVER%==unset (
+if %INPUT%==1 (
+    set TTS_GAMESERVER=127.0.0.1
+) else if %INPUT%==2 (
+    set TTS_GAMESERVER=167.114.220.172
+) else if %INPUT%==3 (
     echo.
     set /P TTS_GAMESERVER=Gameserver: 
+) else exit
+
+echo.
+
+if %INPUT%==2 (
+    set /P ttsUsername="Username: "
+    set /P ttsPassword="Password: "
+) else (
+    set /P TTS_PLAYCOOKIE=Username: 
 )
 
 echo.
-set /P TTS_PLAYCOOKIE=Username: 
 
 echo ===============================
 echo Starting Toontown Stride...
 echo ppython: %PPYTHON_PATH%
-echo Username: %TTS_PLAYCOOKIE%
+
+if %INPUT%==2 (
+    echo Username: %ttsUsername%
+) else (
+    echo Username: %TTS_PLAYCOOKIE%
+)
+
 echo Gameserver: %TTS_GAMESERVER%
 echo ===============================
 
-%PPYTHON_PATH% -m toontown.toonbase.ClientStart
+if %INPUT%==2 (
+    %PPYTHON_PATH% -m toontown.toonbase.ClientStartRemoteDB
+) else (
+    %PPYTHON_PATH% -m toontown.toonbase.ClientStart
+)
+
 pause
