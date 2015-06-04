585dff5132dbc05f29858c63781e565cfba50192
Loudrob <loudhuddles131@yahoo.com>
2015-06-04 19:59:27 -0400

    Default account-server secret for remotedb.

--- toontown/uberdog/ClientServicesManagerUD.py
+++ toontown/uberdog/ClientServicesManagerUD.py
@@ -40,7 +40,7 @@ def entropyIdeal(length):
     return -length * prob * math.log(prob) / math.log(2.0)
 
 accountDBType = config.GetString('accountdb-type', 'developer')
-accountServerSecret = config.GetString('account-server-secret', 'dev')
+accountServerSecret = config.GetString('account-server-secret', '9sj6816aj1hs795j') # Changed in server distribution.
 accountServerHashAlgo = config.GetString('account-server-hash-algo', 'sha512')
 if accountDBType == 'remote':
     if accountServerSecret == 'dev':
