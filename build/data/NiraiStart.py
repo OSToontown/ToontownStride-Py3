13265999055ad3c1db63a209a25f952591b23083
John Cote <jwcotejr@comcast.net>
2015-06-05 23:15:38 -0400

    grammar

--- build/data/NiraiStart.py
+++ build/data/NiraiStart.py
@@ -30,7 +30,7 @@ del dc
 rc4.rc4_setkey('\0\0\0\0')
 
 # Resources
-# TO DO: sign and verify the phases to prevent edition
+# TO DO: Sign and verify the phases to prevent editing.
 
 vfs = VirtualFileSystem.getGlobalPtr()
 mfs = (3, 3.5, 4, 5, 5.5, 6, 7, 8, 9, 10, 11, 12, 13)
