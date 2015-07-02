ff8eade3a586d5f56a0898767cc07e661aef2f09
John <denialmc@outlook.com>
2015-07-02 19:16:23 +0300

    Hide drop shadow for Donald

--- toontown/safezone/DDSafeZoneLoader.py
+++ toontown/safezone/DDSafeZoneLoader.py
@@ -37,6 +37,7 @@ class DDSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
             self.donald.setPos(0, -1, 3.95)
             self.donald.reparentTo(self.boat)
             self.donald.setHat(48, 0, 0)
+            self.donald.hideShadow()
             
             random.shuffle(TTLocalizer.DonaldChatter)
             self.donaldSpeech = self.donald.createTalkSequence(TTLocalizer.DonaldChatter, 15)
