dc81ce63315f8035d2ed83fcd8c79d24dc7f303c
tts <tts@gameserver.toontownstride.com>
2015-07-24 16:54:50 -0400

    stride: add some files for running astron on centos & gameserver on ubuntu (we'll need to switch astron when server chanegs

--- dependencies/astron/darwin/start-ai-server.sh
+++ dependencies/astron/darwin/start-ai-server.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-cd ../..
+cd ../../..
 
-export DYLD_LIBRARY_PATH=`pwd`/Libraries.bundle
-export DYLD_FRAMEWORK_PATH="Frameworks"
+#export DYLD_LIBRARY_PATH=`pwd`/Libraries.bundle
+#export DYLD_FRAMEWORK_PATH="Frameworks"
 
 # Define some constants for our AI server:
 MAX_CHANNELS=999999
 STATESERVER=4002
-ASTRON_IP="127.0.0.1:7100"
-EVENTLOGGER_IP="127.0.0.1:7198"
+ASTRON_IP="158.69.28.83:7100"
+EVENTLOGGER_IP="158.69.28.83:7198"
 
 # Get the user input:
 read -p "District name (DEFAULT: Nuttyboro): " DISTRICT_NAME
@@ -28,7 +28,7 @@ echo "==============================="
 
 while [ true ]
 do
-    ppython -m toontown.ai.ServiceStart --base-channel $BASE_CHANNEL \
+    python -m toontown.ai.ServiceStart --base-channel $BASE_CHANNEL \
                      --max-channels $MAX_CHANNELS --stateserver $STATESERVER \
                      --astron-ip $ASTRON_IP --eventlogger-ip $EVENTLOGGER_IP \
                      --district-name $DISTRICT_NAME
