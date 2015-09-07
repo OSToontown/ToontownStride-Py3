cc823ca360cc1b69a5874c2f811552c7df5767df
root <root@ns501460.ip-192-99-18.net>
2015-09-06 21:54:55 -0400

    fix build scripts on unix

--- build/linux/aes-to-so.sh
+++ build/linux/aes-to-so.sh
@@ -1,5 +1,3 @@
-cd ../nirai/src
+gcc -DNDEBUG -g -O3 -Wall -Wstrict-prototypes -fPIC -DMAJOR_VERSION=1 -DMINOR_VERSION=0 -I/usr/include -I/usr/include/python2.7 -lstdc++ -lssl -lcrypto  -c ../nirai/src/aes.cxx -c -o ../nirai/src/aes.o
 
-gcc -DNDEBUG -g -O3 -Wall -Wstrict-prototypes -fPIC -DMAJOR_VERSION=1 -DMINOR_VERSION=0 -I/usr/include -I/usr/include/python2.7 -lstdc++ -lssl -lcrypto  -c aes.cxx -c -o aes.o
-
-gcc -shared aes.o -L/usr/local/lib -lstdc++ -lssl -lcrypto -o aes.so
+gcc -shared ../nirai/src/aes.o -L/usr/local/lib -lstdc++ -lssl -lcrypto -o ../nirai/src/aes.so
